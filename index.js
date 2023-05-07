e71d8f1c498a923e0baac15cd4a9bd47970b8a3e
Tilki <121335160+codetilki@users.noreply.github.com>
2023-05-07 21:15:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const {AoiClient} = require("aoi.js");
 
+const bot = new AoiClient({
+    token: "Token", //tokeni buraya yaz
+    prefix: "Prefix", //Prefix, örn: "t!"yardım
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+    events: ["onMessage", "onInteractionCreate"],
+    mobilePlatform: true
+});
+
+//durum
+bot.status({
+    text: "Obaa bu bot mobil :D",
+    type: "PLAYING",
+  status: "mobilePlatform",
+    time: 12
+});
+
+// Ping Komutu
+bot.command({
+    name: "ping",
+    code: `Pong! $pingms`
+});
