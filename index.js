dbde6bbf371c462d7d711dc574997ed2d5952966
Tilki <121335160+tilkidevelopment@users.noreply.github.com>
2024-04-03 18:33:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const {AoiClient} = require("aoi.js");
+const aoijs = require("aoi.js")
 
-const bot = new AoiClient({
-    token: "Token", //tokeni buraya yaz
-    prefix: "Prefix", //Prefix, örn: "t!"yardım
-    intents: ["Guilds", "GuildMessages", "MessageContent"],
-    events: ["onMessage", "onInteractionCreate"],
-    mobilePlatform: true
-});
+const bot = new aoijs.Bot({
+token:  process.env.token, //tokeniniz çalınmasın diye gizleyin...
+prefix: ["t!"], //prefix
+intents: ["GUILDS", "GUILD_MESSAGES"] //intent
+})
 
-//durum
-bot.status({
-    text: "Obaa bu bot mobil :D",
-    type: "PLAYING",
-  status: "mobilePlatform",
-    time: 12
-});
+//durum ve değişkenleri ayrı bir dosyanın içinde yapıyorsunuz.
+require('./ayarlar/durum')(bot)
+require('./ayarlar/degiskenler')(bot)
 
-// Ping Komutu
-bot.command({
-    name: "ping",
-    code: `Pong! $pingms`
-});
+
+//Event
+bot.onMessage()
+
+//Komutları dosyada okuma
+const komutlar = new aoijs.LoadCommands(bot);
+komutlar.load(bot.cmd,'./komutlar/') 
+
+//Aşşağıdaki yere isterseniz $log veya $denddm eklebilirsiniz örnek:
+// (Not: ID'niz yerine kendi ID'nizi yazın.)
+bot.readyCommand({
+    channel: "",
+    code: `
+
+ $log[
+  Bot Başarıyla Açıldı!
+  Ad: $userTag[$clientID]
+  Ping: $ping ms
+  Yapımcısı: $username[$botOwnerID]#$discriminator[$botOwnerID]
+  Komut yüklendi: $commandsCount
+  ]
+   `
+})
